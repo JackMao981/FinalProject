75fd6d7d1c928eb9a5a7062c90df7f853d22db16
Lee Smith <smithfeinberg.lee@gmail.com>
2019-04-05 13:19:09 -0400

    Uploading work so far on sprites and bigger everything

---
+++ bigger_main.py
@@ -0,0 +1,129 @@
+"""
+Rogue-like
+@author: Jack Mao and Dieter Brehm
+"""
+
+# grab the pygame library
+import pygame
+
+# grab some system libraries for dealing with sprite files
+import sys
+import os
+import math
+import random
+import bigger_map as map
+from bigger_map import Map
+import bigger_sprite as sprite
+from bigger_sprite import SpriteHandler, WallSprite, FloorSprite, HeroSprite, Characteristics
+
+# Game Class, for handling game loop eventually
+class RogueLike():
+    def __init__(self):
+        """Initialize the roguelike game instance. Handles
+        utility functions necessary for pygame and the
+        main game loop"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((1024, 1024))
+        self.clock = pygame.time.Clock()
+
+        # load the sprite image set
+        self.sprite_handler = SpriteHandler()
+        self.map = Map()
+
+        # define layers
+        self.tile_layers = {
+            "TILE_WALL": pygame.sprite.Group(),
+            "TILE_FLOOR": pygame.sprite.Group(),
+            "TILE_HERO": pygame.sprite.Group(),
+            "TILE_DOOR": pygame.sprite.Group(),
+            "TILE_ITEM": pygame.sprite.Group(),
+            "TILE_ENEMY": pygame.sprite.Group()}
+
+    def spriteRender(self):
+        """Reblit all sprites onto the main screen"""
+
+        # fill the screen with bg color
+        self.screen.fill(pygame.Color(0,0,0))
+
+        # iterate through all tile layers,
+        # drawing the tile specified in sprite
+        # object locations
+        for layer in self.tile_layers:
+            for tile in self.tile_layers[layer]:
+                self.tile_layers["TILE_WALL"].update()
+                self.tile_layers["TILE_FLOOR"].update()
+                self.tile_layers["TILE_HERO"].update()
+                self.tile_layers["TILE_DOOR"].update()
+                self.tile_layers["TILE_ITEM"].update()
+                self.tile_layers["TILE_ENEMY"].update()
+                self.map.viewport_update(self.hero)
+                self.screen.blit(tile.tile, self.map.animator(tile))
+
+        # write changes to screen
+        pygame.display.flip()
+
+    def generate_level(self):
+        """Delete all tiles in desired layer"""
+        for layer in self.tile_layers:
+            for tile in self.tile_layers[layer]:
+                tile.kill()
+
+        #characteristics = Characteristics(616,616,350,66,0,36,1.6, [])
+        #self.hero = HeroSprite(self.tile_layers, self.sprite_handler, (10,10), characteristics)
+        self.hero.posReset((10,10))
+        self.map = Map()
+
+        # place sprites/tiles
+        self.map.generate(self)
+
+    def gameloop(self):
+        """Run the main game loop"""
+
+        # place map related tiles
+        self.map.generate(self)
+
+        # create the hero!
+        characteristics = Characteristics(616,616,350, 350, 66,0,36,1.6, [])
+        self.hero = HeroSprite(self.tile_layers, self.sprite_handler, (10,10), characteristics)
+
+        # run the game loop until program is quit
+        run = True
+        while run:
+            # fetch all events such as keypressed
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    run = False
+                if event.type == pygame.KEYDOWN:
+                    # print the health on every turn
+                    self.hero.characteristics.print_health()
+                    
+                    # collision handler changes reaction based on
+                    # touched tile. Delta change according to
+                    # the direction pressed and is the desired
+                    # movement in units of tiles
+                    if event.key == pygame.K_LEFT:
+                        delta = (-1, 0)
+                        self.hero.collisionHandler(self, delta)
+                    if event.key == pygame.K_RIGHT:
+                        delta = (1, 0)
+                        self.hero.collisionHandler(self, delta)
+                    if event.key == pygame.K_UP:
+                        delta = (0, -1)
+                        self.hero.collisionHandler(self, delta)
+                    if event.key == pygame.K_DOWN:
+                        delta = (0, 1)
+                        self.hero.collisionHandler(self, delta)
+            self.spriteRender()
+
+            # quit the game when the hero's health is 0
+            if(self.hero.characteristics.curr_health <= 0):
+                print("You died.")
+                pygame.quit()
+        pygame.quit()
+
+
+if __name__ == "__main__":
+    # create the main object and run the loop function
+    rogue = RogueLike()
+    rogue.gameloop()
---
+++ bigger_map.py
@@ -0,0 +1,76 @@
+import pygame
+from sprite import *
+import random
+
+# Map Generator Class, for randomly generating a map
+# or for a constant map!
+class Map:
+    def __init__(self):
+        """Create a new handler for map related interactions"""
+        self.viewport = pygame.Rect(0, 0, 1024, 1024)
+        self.width = 1024
+        self.height = 1024
+        self.xset = int((1024 - 256) / 2)
+        self.yset = int((1024 - 256) / 2)
+
+    def animator(self, tile):
+        """Moves tiles according to the top left of the view port
+        which has the offset applied. By changing the viewport
+        when the hero location changes, we can take that and
+        reposition all the other tiles relative to the hero.
+        tile: the sprite object to move"""
+        return tile.rect.move(self.viewport.topleft)
+
+    def viewport_update(self, tile):
+        """Update the position of the viewport. Note that
+        reassigning any of the location attributes of pygame
+        rectangles adjusts all the other attributes and does
+        not resize the rectangle.
+        tile: the sprite object to use to change the viewport"""
+        self.viewport.x = -tile.rect.x + self.xset
+        self.viewport.y = -tile.rect.y + self.xset
+
+    def generate(self, rogue):
+        """Places map sprites
+        rogue: the roguelike game instance"""
+
+        # randomly place floors
+        for x in range(0, 20):
+            for y in range(0,20):
+                if random.random() > 0.2:
+                    FloorSprite(rogue.tile_layers, rogue.sprite_handler, (x, y))
+
+        # randomly place door
+        is_door_placed = False
+        for x in range(0, 20):
+            for y in range(0, 20):
+                if random.random() > 0.5 and not is_door_placed:
+                    is_door_placed = True
+                    DoorSprite(rogue.tile_layers, rogue.sprite_handler, (x, y))
+
+        # randomly place items
+        for x in range(0, 20):
+            for y in range(0, 20):
+                if random.random() < 0.09:
+                    ItemSprite(rogue.tile_layers, rogue.sprite_handler, (x, y))
+        
+        # randomly place enemies
+        for x in range(0, 20):
+            for y in range(0, 20):
+                if random.random() < 0.02:
+                    characteristics = Characteristics(616,616,350,350, 66,0,36,1.6, [])
+                    EnemySprite(rogue.tile_layers, rogue.sprite_handler, (x, y), characteristics)
+
+        # place walls wherever there isn't a floor
+        self.wall_placer(rogue)
+
+    def wall_placer(self, rogue):
+        """place walls wherever there isn't floor"""
+        for x in range(-1,21):
+            for y in range(-1, 21):
+                overlap = False
+                for tile in rogue.tile_layers["TILE_FLOOR"]:
+                    if tile.pos.x == x and tile.pos.y == y:
+                        overlap = True
+                if not overlap:
+                    WallSprite(rogue.tile_layers, rogue.sprite_handler, (x, y))
