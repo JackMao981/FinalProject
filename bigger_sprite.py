3c209ed4d8ed6f825aca9d350c73fa8b4814ba28
melimathlete <melissa.13.anth@gmail.com>
2019-04-24 13:18:19 -0400

    Added sounds to collisionHandler and attack

--- bigger_sprite.py
+++ bigger_sprite.py
@@ -258,8 +258,10 @@ class HeroSprite(pygame.sprite.Sprite):
         self.characteristics.curr_health -= hero_damage_taken
         enemy.characteristics.curr_health -= enemy_damage_taken
         if (enemy.characteristics.curr_health <= 0):
+            enemy_sound.play()
             enemy.kill()
         print("I'm attacking")
+        hit_sound.play()
 
 class EnemySprite(pygame.sprite.Sprite):
     """Class Representing the player,
